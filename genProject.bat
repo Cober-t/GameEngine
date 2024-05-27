0cf9c8e195e234599925707954a620c4d55f125a
Cober-t <jorgeouscober@gmail.com>
2024-05-27 07:39:20 +0200

    [Add] Cleaned up main brach on preparations to the merge

--- genProject.bat
+++
@@ -1,3 +0,0 @@
-set VS_PATH = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
-
-Start "" "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" "."
